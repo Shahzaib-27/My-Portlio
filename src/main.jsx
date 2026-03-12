ae076c865a3648155a83a194c8ae3aeeefc439ff
Shahzaib <shazuthebigbrother@gmail.com>
2026-03-12 23:30:41 +0500

    Analytics enable

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { Analytics } from "@vercel/analytics/react"
 
 import App from './App.jsx'
 import './index.css'
@@ -8,6 +7,5 @@ import './index.css'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-    <Analytics />
   </React.StrictMode>,
 )
